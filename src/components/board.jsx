6ee610d0ed4f68d78fc0af95a8cdddb5c72ca1c6
Stanislav <s.v.pukha@gmail.com>
2022-04-05 15:45:46 +0300

    alarm

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -3,13 +3,6 @@ import Square from './square';
 
 
 export default class Board extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  }
 
   handleClick(i){
     const squares = this.state.squares.slice()
@@ -25,24 +18,16 @@ export default class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square 
-        value={this.state.squares[i]}
-        onClick={()=>this.handleClick(i)}
-        cls ={this.state.xIsNext ? 'redX' : 'blueO'}
+        value={this.props.squares[i]}
+        onClick={()=>this.props.onClick(i)}
+        cls ={this.state.xIsNext ? 'redX' : 'blueO'}// this is for colors of x and o, dont know how to handle this
       />)
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares)
-    let status
-    if(winner){
-      status = 'The winner is ' + winner
-    } else{
-    status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
-    }
     return (
       <div>
         <h1 className='title'>TIC TAC TOE</h1>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -63,13 +48,3 @@ export default class Board extends React.Component {
   }
 }
 
-function calculateWinner(squares) {
-  const lines = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6],]
-  for(let i=0; i<lines.length; i++){
-    const [a, b, c] = lines[i]
-    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
-    }
-  }
-  return null
-}
